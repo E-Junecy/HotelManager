dff7b057a2c64d6358cb6024cc41bfc3da54561f
E-Junecy <dl3282553131@163.com>
2026-03-30 17:14:44 +0800

    room module compile

---
+++ room.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include"room.h"
+
+room_type room::get_type(){
+    return type;
+}
+room_status room::get_status(){
+    return status;
+}
+int room::get_num(){
+    return room_num;
+}
+vector<int> room::get_guest(){
+    return guest_in_house;
+}
+void room::add_guest(int ID){
+    status=stayed;
+    guest_in_house.push_back(ID);
+}
+void room::reserve_guest(int ID){
+    status=reserved;
+    guest_in_house.push_back(ID);
+}
+void room::clear_guest(){
+    status=no_guest;
+    guest_in_house.clear();
+}
+room::room(){
+    type=single;
+    status=no_guest;
+    room_num=0;
+}
--- room.h
+++ room.h
@@ -1,3 +1,36 @@
 #pragma once
-
+#include<vector>
 using namespace std;
+
+//定义房间类型枚举类型
+enum room_type{
+    single,
+    standard,
+    suite,
+    family,
+};
+
+//定义房间状态枚举类型
+enum room_status{
+    no_guest,
+    stayed,
+    reserved,
+};
+
+//客房基本信息
+class room{
+private:
+    room_type type;
+    room_status status;
+    int room_num;
+    vector<int> guest_in_house;
+public:
+    room_type get_type();
+    room_status get_status();
+    int get_num();
+    vector<int> get_guest();
+    void add_guest(int ID);
+    void reserve_guest(int ID);
+    void clear_guest();
+    room::room();
+};
