7e53131c87d279d27bb919d205ec228a110bca1d
E-Junecy <dl3282553131@163.com>
2026-03-30 21:32:57 +0800

    manager module head file compile

--- manager.h
+++ manager.h
@@ -5,6 +5,23 @@
 #include"room.h"
 using namespace std;
 
-vector<class room> hotel_room;
-unordered_map<int,class guest_info> hotel_guest;
-unordered_map<int,class checkin_record> guest_record;
+class manager{
+private:
+    vector<class room> hotel_room;
+    unordered_map<int,class guest_info> hotel_guest;
+    unordered_map<int,class checkin_record> guest_record;
+
+public:
+    bool add_room(room room);
+    bool delete_room(int room_num);
+
+    vector<int> get_empty_room();
+    vector<int> get_empty_room(room_type type);
+
+    void guest_check_in(int ID,int room_num);
+    void guest_reserve(int ID,int room_num);
+    void guest_check_out(int ID,int room_num);
+
+    void get_guest_info(int room_num);
+    void get_guest_stay(int room_num);
+};
